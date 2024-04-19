6106302e0a30d7ab5632110b54b581e8f37d8ca0
William <titanbabrisa@gmail.com>
2024-04-18 23:49:08 -0400

    changes

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -20,7 +20,7 @@ def get_username(id):
         signal = requests.get(route)
         if signal.status_code == 200:
             data = signal.json()
-            return data[0].get('username', '')
+            return data[0].get('username', 'Not Found')
     else:
         raise ValueError("Fail, requesting data for given ID")
 
@@ -32,31 +32,44 @@ def format_for_json(data=None):
         Returns:
             A dict with the data
     """
+
+    userId = data[0].get('userId', '')
+    username = get_username(userId)
+    core_dict = {str(userId): []}
     for info in data:
-        userId = data[0].get('userId', '')
-        username = get_username(userId)
-        core_dict = {str(userId): []}
-        while(info.get('userId', None) != userId):
-            title = info.get('title', 'Not Found')
-            status = info.get('completed', 'Not Found')
-            temp = {"username": username, "task": title, "completed": status}
-            core_dict[str(userId)].append(temp.copy())
-    return core_dict
+        title = info.get('title', 'Not Found')
+        status = info.get('completed', 'Not Found')
+        temp = {"username": username, "task": title, "completed": status}
+        core_dict[str(userId)].append(temp.copy())
+    employee_id = list(core_dict.keys())
+    data_list = core_dict[str(userId)]
+    return (employee_id[0], data_list)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        emplo_id = sys.argv[1]
-    else:
-        raise ValueError(f"request failed, no employee ID found")
-    url = f"https://jsonplaceholder.typicode.com/todos/"
-    connection = requests.get(url)
-    if connection.status_code == 200:
-        data = connection.json()
-        info = format_for_json(data)
-        filename = "todo_all_employees.json"
-        with open(filename, 'w') as file:
-            json.dump(info, file, indent=2)
-    else:
-        raise requests.exceptions.HTTPError(
-            f"request failed, status code: {connection.status_code}")
+    info = {}
+    indx = 1
+    while True:
+        route = f"https://jsonplaceholder.typicode.com/users?id={indx}"
+        links = requests.get(route)
+        if links.status_code == 200:
+            lista = links.json()
+        else:
+            raise requests.exceptions.HTTPError(
+                f"request failed, status code: {links.status_code}")
+        if lista != []:
+            url = f"https://jsonplaceholder.typicode.com/todos?userId={indx}"
+            connection = requests.get(url)
+            if connection.status_code == 200:
+                data = connection.json()
+                key_value = format_for_json(data)
+                info[key_value[0]] = key_value[1]
+            else:
+                raise requests.exceptions.HTTPError(
+                    f"request failed, status code: {connection.status_code}")
+            indx += 1
+        else:
+            break
+    filename = "todo_all_employees.json"
+    with open(filename, 'w') as file:
+        json.dump(info, file, indent=2)
